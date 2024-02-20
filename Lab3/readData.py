287d1497066fe408fbdd4dee1d4c71844be4a3cd
erikwkv <erikkvaloy@gmail.com>
2024-02-20 18:07:06 +0100

    puls målt

--- Lab3/readData.py
+++ Lab3/readData.py
@@ -4,16 +4,18 @@ import numpy as np
 
 # Read the data from the file
 y = pd.read_csv('tommel2', sep=' ')
-print(y)
-
-red = y[:,0]
-green = y[:,1]
-blue = y[:,2]
-time = np.linspace(0,10,len(green))
 
 # Plot the data
-plt.plot(time, green, 'o', label='Data from tommel2')
+# Plot the data
+time = np.linspace(0,10,len(y.iloc[:, 1]))
+plt.plot(time,y.iloc[:, 1], label='Data from tommel2')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Data from tommel2')
 plt.show()
+
+# # Plot the data
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.title('Data from tommel2')
+# plt.show()
