3534a2476f5919713727d8d45e5c6c99bf74cd45
Benjamin-A <benjamin.roald.andersen@gmail.com>
2024-02-20 18:22:02 +0100

    normalised

--- Lab3/readData.py
+++ Lab3/readData.py
@@ -11,8 +11,8 @@ red = y.iloc[:, 0]
 green = y.iloc[:, 1]
 blue = y.iloc[:, 2]
 
-
-time = np.linspace(0,10,len(green))
+videolength = 10
+time = np.linspace(0,videolength,len(green))
 plt.plot(time,red, label='red', color='red')
 plt.plot(time,green, label='green', color='green')
 plt.plot(time,blue, label='blue', color='blue')
@@ -20,3 +20,17 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Data from tommel2')
 plt.show()
+
+
+#plot with normalised data
+red = red-np.max(red)
+green = green-np.max(green)
+blue = blue-np.max(blue)
+plt.plot(time,red, label='red', color='red')
+plt.plot(time,green, label='green', color='green')
+plt.plot(time,blue, label='blue', color='blue')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Data from tommel2')
+plt.show()
+
