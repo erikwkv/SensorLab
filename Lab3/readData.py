ac7a47f0c384b61df772a8da7b8354faf0735806
erikwkv <erikkvaloy@gmail.com>
2024-02-20 18:02:52 +0100

    csv read

--- Lab3/readData.py
+++ Lab3/readData.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Read the data from the file
-data = pd.read_csv('Lab3/tommel2', sep=' ')
+y = pd.read_csv('tommel2', sep=' ')
+print(y)
+
+red = y[:,0]
+green = y[:,1]
+blue = y[:,2]
+time = np.linspace(0,10,len(green))
 
 # Plot the data
-plt.plot(data['x'], data['y'])
+plt.plot(time, green, 'o', label='Data from tommel2')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Data from tommel2')
