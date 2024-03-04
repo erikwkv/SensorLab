b348c4f55ebfba8d32afe66d691c62acd11a8b75
Benjamin-A <benjamin.roald.andersen@gmail.com>
2024-03-04 16:47:43 +0100

    added markings to plot++

--- Lab4/testplot-Simdata.py
+++ Lab4/testplot-Simdata.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import pandas as pd
+import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -17,13 +19,21 @@ labels = data.columns[[1, 3]]
 
 
 # Plot the data
-plt.plot(x, y1)
-plt.plot(x, y2)
+fig, ax = plt.subplots(1,figsize=(10,10))
+
+ax.axhline(y=1,color='red',linestyle='--', label=data.columns[2])
+plt.plot(x, y1, label= labels[0])
+plt.plot(x, y2, label= labels[1])
 plt.xlabel('Frequency')
-plt.ylabel('Amplitude relative to input')
+plt.ylabel('Amplitude relative to {0}'.format(data.columns[2]))
 plt.xscale('log')  # Set x-axis to logarithmic scale
 plt.yscale('log')  # Set x-axis to logarithmic scale
 plt.title('Simulated Bode Plot')
-plt.legend(labels)
+plt.legend(bbox_to_anchor=(0.5, -0.15), loc="upper center",fancybox=True, ncol=5, borderaxespad=0)
+plt.tight_layout(rect=[0,0,1,0.98])
+
 plt.grid(True)
+
+# Add a horizontal line at y=1, representing the input
+  
 plt.show()
