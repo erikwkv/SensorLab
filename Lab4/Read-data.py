85e224bd5c72da54ef360978b3ad5743ae69d541
erikwkv <erikkvaloy@gmail.com>
2024-04-15 16:11:41 +0200

    Rydding av read-data.py

--- Lab4/Read-data.py
+++ Lab4/Read-data.py
@@ -1,82 +1,64 @@
 import matplotlib.pyplot as plt
 from raspi_import import raspi_import
 import numpy as np
-from scipy.signal import find_peaks
 
-sample_period, data = raspi_import('Lab4/kjapp1.bin')
+def get_IFI_IFQ_fft(filename):
+    sample_period, data = raspi_import('Lab4/kjapp2.bin')
 
-# Plotting the data
-time_axis = 1e3*sample_period*np.arange(len(data))
+    radar_IF_I = (data[1:,4] - np.mean(data[:,1]))*0.00081
+    radar_IF_Q = (data[1:,2] - np.mean(data[:,2]))*0.00081
 
-radar_IF_I = (data[1:,4] - np.mean(data[:,1]))*0.00081
-radar_IF_Q = (data[1:,2] - np.mean(data[:,2]))*0.00081
+    #remove dc offset
+    radar_IF_I = radar_IF_I - np.mean(radar_IF_I)
+    radar_IF_Q = radar_IF_Q - np.mean(radar_IF_Q)
 
+    x = radar_IF_I + 1j*radar_IF_Q
 
+    #windowing
+    window = np.hanning(len(x))
+    radar_IF_I = radar_IF_I*window
+    x = x*window
+    #fft
+    X = np.fft.fft(x,2**23)
+    freqz = np.fft.fftfreq(2**23,sample_period)
 
-time_axis = 1e3*sample_period*np.arange(len(data)-1)
+    return X, freqz, x, radar_IF_I, radar_IF_Q, sample_period
 
+def plot_time_domain(x, radar_IF_I, radar_IF_Q,sample_period):
 
-#remove dc offset
-radar_IF_I = radar_IF_I[0:54000] - np.mean(radar_IF_I)
-radar_IF_Q = radar_IF_Q[0:54000] - np.mean(radar_IF_Q)
-
-x = radar_IF_I + 1j*radar_IF_Q
-
-# plt.plot(radar_IF_I,label='IF_I',color='b')
-# plt.plot(radar_IF_Q,label='IF_Q',color='r')
-# plt.xlabel('Time (ms)')
-# plt.ylabel('Amplitude (V)')
-# plt.legend()
-# plt.show()
-
-# z = z[0:54000]
-
-# print(len(radar_IF_I))
-# radar_IF_I = radar_IF_I[10_000:25_000]
-
-
-# plt.plot(radar_IF_I,label='IF_I',color='b')
-# plt.xlabel('Time (ms)')
-# plt.ylabel('Amplitude (V)')
-# plt.legend()
-# plt.show()
+    time_axis = 1e3*sample_period*np.arange(len(radar_IF_I))
+    plt.plot(time_axis,radar_IF_Q,label='IF_Q',color='r')
+    plt.plot(time_axis,radar_IF_I,label='IF_I',color='b')
+    plt.xlabel('Time (ms)')
+    plt.ylabel('Amplitude (V)')
+    plt.legend()
+    plt.show()
 
 
-#windowing
-window = np.hanning(len(x))
-radar_IF_I = radar_IF_I*window
-x = x*window
-#fft
-X = np.fft.fft(x,2**23)
-freqz = np.fft.fftfreq(2**23,sample_period)
-fft_radar_IF_I = np.fft.fft(radar_IF_I,n=len(radar_IF_I))
-freq = np.fft.fftfreq(len(radar_IF_I),sample_period)
-
-plt.plot(freqz,20*np.log10(np.abs(X)),label='X',color='r')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude (dB)')
-plt.grid()
-plt.legend()
-plt.show()
+def plot_freq_domain(X,freqz):
+    plt.plot(freqz,20*np.log10(np.abs(X)))
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude (dB)')
+    plt.grid()
+    plt.show()
 
 
-#find peaks between -15hz to -5hz
-# peaks_I, _ = find_peaks(20*np.log10(np.abs(Z)))
-# peaks_Q, _ = find_peaks(20*np.log10(np.abs(fft_radar_IF_Q)),height=55)
+def find_freq(X,freqz):
+    peak_index = np.argmax(np.abs(X))
+    f_d = freqz[peak_index]
+    return f_d
 
-peak_index = np.argmax(np.abs(X))
-f_d = freqz[peak_index]
-
-# peaks, _ = find_peaks(20*np.log10(np.abs(X)),height=51.7)
-
-# freq_peaks = freqz[peaks]
-
-# print(freq_peaks)
-
-f_0 = 24.13*10**9
-
-def velocity(f_d,f_0): 
+def velocity(f_d): 
+    f_0 = 24.13*10**9
     c = 3*10**8
     return ((c*f_d) / (2*f_0))
 
-print(velocity(f_d,f_0))
+X, freqz, x, radar_IF_I, radar_IF_Q, sample_period = get_IFI_IFQ_fft('Lab4/kjapp2.bin')
+
+plot_time_domain(x, radar_IF_I, radar_IF_Q,sample_period)
+plot_freq_domain(X,freqz)
+f_d = find_freq(X,freqz)
+print('f_d:',f_d)
+v = velocity(f_d)
+print('velocity:',v)
+
