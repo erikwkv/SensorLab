9c42ce220abb97a27e5dcc71521f8a2558f3a44d
erikwkv <erikkvaloy@gmail.com>
2024-04-22 14:21:09 +0200

    Monday work

--- Lab4/Read-data.py
+++ Lab4/Read-data.py
@@ -3,10 +3,10 @@ from raspi_import import raspi_import
 import numpy as np
 
 def get_IFI_IFQ_fft(filename):
-    sample_period, data = raspi_import('Lab4/kjapp2.bin')
+    sample_period, data = raspi_import(filename)
 
-    radar_IF_I = (data[1:,4] - np.mean(data[:,1]))*0.00081
-    radar_IF_Q = (data[1:,2] - np.mean(data[:,2]))*0.00081
+    radar_IF_I = (data[1:,4])*0.00081
+    radar_IF_Q = (data[1:,2])*0.00081
 
     #remove dc offset
     radar_IF_I = radar_IF_I - np.mean(radar_IF_I)
@@ -19,16 +19,16 @@ def get_IFI_IFQ_fft(filename):
     radar_IF_I = radar_IF_I*window
     x = x*window
     #fft
-    X = np.fft.fft(x,2**23)
-    freqz = np.fft.fftfreq(2**23,sample_period)
+    X = np.fft.fft(x)
+    freqz = np.fft.fftfreq(len(x),sample_period)
 
     return X, freqz, x, radar_IF_I, radar_IF_Q, sample_period
 
 def plot_time_domain(x, radar_IF_I, radar_IF_Q,sample_period):
 
     time_axis = 1e3*sample_period*np.arange(len(radar_IF_I))
-    plt.plot(time_axis,radar_IF_Q,label='IF_Q',color='r')
-    plt.plot(time_axis,radar_IF_I,label='IF_I',color='b')
+    plt.plot(radar_IF_Q,label='IF_Q',color='r')
+    plt.plot(radar_IF_I,label='IF_I',color='b')
     plt.xlabel('Time (ms)')
     plt.ylabel('Amplitude (V)')
     plt.legend()
@@ -39,6 +39,7 @@ def plot_freq_domain(X,freqz):
     plt.plot(freqz,20*np.log10(np.abs(X)))
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Magnitude (dB)')
+    plt.xlim(-5000,5000)
     plt.grid()
     plt.show()
 
@@ -53,12 +54,34 @@ def velocity(f_d):
     c = 3*10**8
     return ((c*f_d) / (2*f_0))
 
-X, freqz, x, radar_IF_I, radar_IF_Q, sample_period = get_IFI_IFQ_fft('Lab4/kjapp2.bin')
 
-plot_time_domain(x, radar_IF_I, radar_IF_Q,sample_period)
-plot_freq_domain(X,freqz)
-f_d = find_freq(X,freqz)
-print('f_d:',f_d)
-v = velocity(f_d)
-print('velocity:',v)
+def hele(filename):
+    X, freqz, x, radar_IF_I, radar_IF_Q, sample_period = get_IFI_IFQ_fft(filename)
+    f_d = find_freq(X,freqz)
+    print('f_d:',f_d)
+    v = velocity(f_d)
+    print('velocity:',v)
+    plot_time_domain(x, radar_IF_I, radar_IF_Q,sample_period)
+    plot_freq_domain(X,freqz)
+
+hele('Lab4/sakte2.bin')
+
+# print('Sakte:')
+# hele('Lab4/sakte1.bin')
+# hele('Lab4/sakte2.bin')
+# hele('Lab4/sakte3.bin')
+# hele('Lab4/sakte4.bin')
+
+# print('Kjapp:')
+# hele('Lab4/kjapp1.bin')
+# hele('Lab4/kjapp2.bin')
+# hele('Lab4/kjapp3.bin')
+# hele('Lab4/kjapp4.bin')
+
+# print('Rygging:')
+# hele('Lab4/rygging1.bin')
+# hele('Lab4/rygging2.bin')
+# hele('Lab4/rygging3.bin')
+# hele('Lab4/rygging4.bin')
+
 
